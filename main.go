9937ff49d21895c68e6c8d5b4e065a1a70c65400
Les Aker <me@lesaker.org>
2014-06-30 00:47:28 -0400

    better error handling for extraction

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"github.com/dotcloud/docker/archive"
-	"io"
-	"net/http"
-	"os"
+    "fmt"
+    "github.com/dotcloud/docker/archive"
+    "io"
+    "net/http"
+    "os"
     "crypto/tls"
 )
 
 func usage() {
-	fmt.Printf("Usage: %s https://example.org/download.tar.bz2\n", os.Args[0])
-	os.Exit(1)
+    fmt.Printf("Usage: %s https://example.org/download.tar.bz2\n", os.Args[0])
+    os.Exit(1)
 }
 
 func download(url string) io.Reader {
@@ -19,18 +19,21 @@ func download(url string) io.Reader {
         TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
     }
     client := &http.Client{Transport: transport}
-	response, err := client.Get(url)
-	if err != nil {
-		fmt.Printf("Failed to download %s -- %s\n", url, err)
-		os.Exit(1)
-	}
-	fmt.Printf("Downloaded %s\n", url)
-	return response.Body
+    response, err := client.Get(url)
+    if err != nil {
+        fmt.Printf("Failed to download %s -- %s\n", url, err)
+        os.Exit(1)
+    }
+    fmt.Printf("Downloaded %s\n", url)
+    return response.Body
 }
 
 func main() {
-	if len(os.Args) < 2 {
-		usage()
-	}
-	archive.Untar(download(os.Args[1]), "/", nil)
+    if len(os.Args) < 2 {
+        usage()
+    }
+    err := archive.Untar(download(os.Args[1]), "/", nil)
+    if err != nil {
+        fmt.Printf("Failed to extract -- %s\n", err)
+    }
 }
